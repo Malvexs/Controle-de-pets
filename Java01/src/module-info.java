03d78bff499b36c22a372201148f6a7c9fe376c3
BlackbsD <36817245+BlakbsD@users.noreply.github.com>
2022-10-30 23:02:39 -0300

    Commit 01

---
+++ Java01/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author marce
+ *
+ */
+module Java01 {
+}
---
+++ Java01/src/principal/main.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola mundo");
+	}
+
+}
